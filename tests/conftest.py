02bc9529c2605542bdbc5716a36cb93f6ec84f07
frozze <darkfrozen659@gmail.com>
2025-12-16 22:33:59 +0200

    {6.5 Фикстуры Pytest}. {Практика работы с pytest фикстурами}

---
+++ tests/conftest.py
@@ -0,0 +1,55 @@
+import pytest  # Импортируем pytest
+from playwright.sync_api import Playwright, Page  # Имопртируем класс страницы, будем использовать его для аннотации типов
+
+
+@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
+def chromium_page(playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
+    # Ниже идет инициализация и открытие новой страницы
+    # Запускаем браузер
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    # Передаем страницу для использования в тесте
+    yield page
+
+    # Закрываем браузер после выполнения тестов
+    context.close()
+    browser.close()
+
+@pytest.fixture(scope='session')
+def initialize_browser_state(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto(
+        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    context.storage_state(path="browser-state.json")
+    context.close()
+    browser.close()
+
+@pytest.fixture(scope='function')
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    """Страница, открытая с уже сохранённым состоянием браузера"""
+
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+
+    yield page
+
+    context.close()
+    browser.close()
--- tests/test_coureses.py
+++ tests/test_coureses.py
@@ -1,53 +1,23 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import Page, expect
 
-# Открываем браузер с использованием Playwright
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        # Запускаем Chromium браузер в обычном режиме (не headless)
-        browser = playwright.chromium.launch(headless=False)
-        # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
-        context = browser.new_context()
-        # Открываем новую страницу в рамках контекста
-        page = context.new_page()
+def test_empty_courses_list(chromium_page_with_state:Page):
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
+    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text("Courses")
 
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
+    no_result_block = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(no_result_block).to_be_visible()
+    expect(no_result_block).to_have_text("There is no results")
 
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
+    empty_block_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_block_icon).to_be_visible()
 
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
-        context.storage_state(path="browser-state.json")
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json") # Указываем файл с сохраненным состоянием
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-        courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses_title).to_be_visible()
-        expect(courses_title).to_have_text("Courses")
-
-        no_result_block = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(no_result_block).to_be_visible()
-        expect(no_result_block).to_have_text("There is no results")
-
-        empty_block_icon = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(empty_block_icon).to_be_visible()
-
-        empty_view_description_text = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(empty_view_description_text).to_be_visible()
-        expect(empty_view_description_text).to_have_text("Results from the load test pipeline will be displayed here")
+    empty_view_description_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description_text).to_be_visible()
+    expect(empty_view_description_text).to_have_text("Results from the load test pipeline will be displayed here")
